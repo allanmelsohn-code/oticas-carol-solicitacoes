9e9b387015c64261c3e5945734eb4131276de129
Allan Melsohn <allanmelsohn@gmail.com>
2026-05-03 14:16:35 -0300

    feat: atualizar ícone do app para novo design

--- scripts/generate-icons.mjs
+++ scripts/generate-icons.mjs
@@ -1,6 +1,6 @@
 import sharp from 'sharp'
 
-const source = 'Gemini_Generated_Image_i4cgy5i4cgy5i4cg.png'
+const source = 'Gemini_Generated_Image_3rzf7d3rzf7d3rzf.png'
 
 await sharp(source).resize(192, 192).toFile('public/icon-192x192.png')
 await sharp(source).resize(512, 512).toFile('public/icon-512x512.png')
